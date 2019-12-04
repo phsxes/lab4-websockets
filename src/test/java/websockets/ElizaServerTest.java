b86912e3e308d44a3125209b6154e60e3aaaf09f
Julio Salceda <julio.salceda@cetys.edu.mx>
2019-12-04 19:30:29 +0100

    Completed onChat test

--- src/test/java/websockets/ElizaServerTest.java
+++ src/test/java/websockets/ElizaServerTest.java
@@ -56,16 +56,16 @@ public class ElizaServerTest {
 	}
 
 	@Test(timeout = 1000)
-	@Ignore
 	public void onChat() throws DeploymentException, IOException, URISyntaxException, InterruptedException {
-		// COMPLETE ME!!
+        CountDownLatch latch = new CountDownLatch(4);
 		List<String> list = new ArrayList<>();
 		ClientEndpointConfig configuration = ClientEndpointConfig.Builder.create().build();
 		ClientManager client = ClientManager.createClient();
-		client.connectToServer(new ElizaEndpointToComplete(list), configuration, new URI("ws://localhost:8025/websockets/eliza"));
-		// COMPLETE ME!!
-		// COMPLETE ME!!
-		// COMPLETE ME!!
+		Session session = client.connectToServer(new ElizaEndpointToComplete(list, latch), configuration, new URI("ws://localhost:8025/websockets/eliza"));
+        session.getAsyncRemote().sendText("How are you?");
+        latch.await();
+        assertEquals("We were discussing you, not me.", list.get(3));
+
 	}
 
 	@After
@@ -94,16 +94,16 @@ public class ElizaServerTest {
     private static class ElizaEndpointToComplete extends Endpoint {
 
         private final List<String> list;
+        private final CountDownLatch latch;
 
-        ElizaEndpointToComplete(List<String> list) {
+        ElizaEndpointToComplete(List<String> list, CountDownLatch latch) {
             this.list = list;
+            this.latch = latch;
         }
 
         @Override
         public void onOpen(Session session, EndpointConfig config) {
-
-            // COMPLETE ME!!!
-
+            LOGGER.info(format("Opened Connection"));
             session.addMessageHandler(new ElizaMessageHandlerToComplete());
         }
 
@@ -112,7 +112,7 @@ public class ElizaServerTest {
             @Override
             public void onMessage(String message) {
                 list.add(message);
-                // COMPLETE ME!!!
+                latch.countDown();
             }
         }
     }
